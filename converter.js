c38d1dc7b810aa49f30004e9a32c594e18212269
Callan Morrison <callan.l.morrison@gmail.com>
2016-02-09 22:24:36 -0600

    added basic conversion functions

--- converter.js
+++ converter.js
@@ -1,13 +1,25 @@
-function toCelsius () {
+// Select User input
+var toConvert = document.getElementById("toConvert").value;
+console.log("toConvert", toConvert);
 
+
+
+// T(°C) = (T(°F) - 32) × 5/9
+function toCelsius (fValue) {
+    var cValue = (fValue - 32) * (5/9);
+    return cValue;
 }
 
-function toFahrenheit () {
-
+// T(°F) = T(°C) × 9/5 + 32
+function toFahrenheit (cValue) {
+    var fValue = (cValue * (9/5)) + 32;
+    return fValue;
 }
 
 // Get a reference to the button element in the DOM
 var button = document.getElementById("converter");
+button.onclick = determineConverter;
+
 
 // This function should determine which conversion should
 // happen based on which radio button is selected.
